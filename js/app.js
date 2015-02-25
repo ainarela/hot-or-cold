984299c20a302c73e36b2df01f546c57117ac79e
Aina Requena <aina.requena@gmail.com>
2015-02-25 18:43:53 +0100

    Added functions to start a new game and to provide feedback

--- js/app.js
+++ js/app.js
@@ -12,6 +12,61 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+  	/* HOT OR COLD GAME
+  	------------------------------------------------*/
+
+  	// Generate secret integer between 1 and 100 when the page loads
+  	var secretNum = Math.floor(Math.random()*100)+1;
+  	console.log(secretNum);
+
+  	// Start a new game
+  	$(".new").click(newGame);
+
+  	// Give feedback to the user when a new value is submitted
+  	$("#guessButton").click( function submitValue(e) {
+  		e.preventDefault();
+  		feedback(secretNum);
+  	});
+
+  	// Function that reloads the page in order to start a new game  
+  	function newGame(){
+  		location.reload();
+  	}
+
+  	// Function that gives the appropiate feedback to the user input
+  	function feedback(num){
+
+  		var input = $("#userGuess").val();
+  		var distance = Math.abs(num - (+input));
+
+  		if (distance > 0 && distance <= 10) {
+  			alert("Very hot");
+  		}
+  		else if (distance > 10 && distance <= 20) {
+  			alert("Hot");
+  		}
+  		else if (distance > 20 && distance <= 30) {
+  			alert("Warm");
+  		}
+  		else if (distance > 30 && distance <= 50) {
+  			alert("Cold");
+  		}
+  		else if (distance > 50) {
+  			alert("Ice cold");
+  		}
+  		else if (distance == 0) {
+  			alert("Congratulations!");
+  		}
+  		$("#userGuess").val("");
+
+  	}
+
+
+  	
+
+
+
 });
 
 
